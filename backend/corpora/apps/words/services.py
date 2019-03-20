da8a925c85ae88e8c8e1b04e20891ea67303f5f8
Ariel Burriss <ariburr@gmail.com>
2019-03-20 15:21:41 -0600

    add word length

--- backend/corpora/apps/words/services.py
+++ backend/corpora/apps/words/services.py
@@ -8,13 +8,14 @@ class WordService(object):
         vectorizer = CountVectorizer(
             token_pattern=r"\b[\w']+\b", analyzer="word")
         tokens = vectorizer.fit([text]).get_feature_names()
+        length = [len(t) for t in tokens]
         freq = vectorizer.transform([text]).toarray()[0].tolist()
         lang = [language_identifier] * len(tokens)
-        data = [list(t) for t in zip(tokens, freq, lang)]
+        data = [list(t) for t in zip(tokens, length, freq, lang)]
         with connection.cursor() as cursor:
             cursor.executemany(
-                "INSERT INTO words_word(word,count,language_id)\
-                VALUES (%s,%s,%s) ON CONFLICT (word)\
+                "INSERT INTO words_word(word,count,length,language_id)\
+                VALUES (%s,%s,%s,%s) ON CONFLICT (word)\
                 DO UPDATE SET count = excluded.count + words_word.count;", data)
             cursor.execute(
                 "WITH new_values AS (SELECT id, count / (SELECT SUM(count)::FLOAT FROM words_word) AS freq FROM words_word)\
